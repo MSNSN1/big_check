242f918abafc63b5ec9bd6cd8a403f8571259ce6
bo-oz <boristoet80@gmail.com>
2019-01-12 15:10:42 +0100

    Fix

--- lib/big_check/client.rb
+++ lib/big_check/client.rb
@@ -3,9 +3,26 @@ module BigCheck
     ENDPOINT = 'http://webservices.cibg.nl/Ribiz/OpenbaarV4.asmx?wsdl'
     require 'savon'
 
-    def create_connection
-      return
+    def initialize
+      @client = Savon.client(wsdl: ENDPOINT, log: false)
+    end
+
+    def check_by_big(big)
+      message = {
+        "WebSite" => "Ribiz",
+        "RegistrationNumber" => big_number
+      }
+      begin
+        response = @client.call(:list_hcp_approx3, message: message)
+        format_response(response)
+      rescue Savon::SOAPFault => e
+        raise NotFoundError.new(e.message)
+      end
     end
 
   end
+
+  class NotFoundError < StandardError
+  end
+
 end
